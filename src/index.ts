0cc15413bb6800b0de798046ab5fe00934699bd3
devvagner <wagnerluizsg@outlook.com.br>
2023-09-16 09:02:41 -0300

    :sparkles: feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from "cors"
+
+
+const port = process.env.PORT || 4242;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen( port, () => {
+    if ( server ) console.log( `🚀 The server is running on localhost:${port}` );
+    else console.log( 'Error running the server' );
+} );
